ed67b6bb928ea18cc6a1f003c51886278a9b2f54
Gienadij Mackiewicz <gienadijmackiewicz@gmail.com>
2020-08-10 14:37:27 +0200

    Add missing Int & Double extensions

---
+++ Sources/UIDevice+Extension/Double+Extension.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+public extension Double {
+    ///  Public computed property to scale layout constants for small devices
+    var deviceSizeAware: CGFloat {
+        return UIDevice.current.isSmallDevice ? (CGFloat(self) * 0.8) : CGFloat(self)
+    }
+}
