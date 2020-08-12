d664922c6d8d34809812ccc603ee8acac63cc06b
Gienadij Mackiewicz <gienadijmackiewicz@gmail.com>
2020-08-12 23:48:24 +0200

    Add rounding to Int & Double extension

--- Sources/UIDevice+Extension/Int+Extension.swift
+++ Sources/UIDevice+Extension/Int+Extension.swift
@@ -3,6 +3,6 @@ import UIKit
 public extension Int {
     ///  Public computed property to scale layout constants for small devices
     var deviceSizeAware: CGFloat {
-        return UIDevice.current.isSmallDevice ? (CGFloat(self) * 0.8) : CGFloat(self)
+        return UIDevice.current.isSmallDevice ? (CGFloat(self) * 0.8).rounded() : CGFloat(self)
     }
 }
